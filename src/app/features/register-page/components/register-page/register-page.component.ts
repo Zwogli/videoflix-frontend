628bbf9ab69715ca9bb44f9a07adf894103adad1
Zwogli <mathiaskohler@web.de>
2024-07-04 10:21:37 +0200

    Edit responsive view

--- src/app/features/register-page/components/register-page/register-page.component.ts
+++ src/app/features/register-page/components/register-page/register-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreakpointObserverService } from 'src/app/core/breakpoint-observer/breakpoint-observer.service';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  styleUrls: ['./register-page.component.scss'],
 })
 export class RegisterPageComponent {
+  isXSmall$!: Observable<boolean>;
+  isXLarge$!: Observable<boolean>;
 
+  constructor(private breakpointObserverService: BreakpointObserverService) {}
+
+  ngOnInit(): void {
+    this.isXSmall$ = this.breakpointObserverService.isXSmall();
+    this.isXLarge$ = this.breakpointObserverService.isXLarge();
+    console.log('isXSmall$', this.isXSmall$);
+  }
 }
