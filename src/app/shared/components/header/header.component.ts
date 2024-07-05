7fd542444a9df67b95769807811bec6faaab011b
Zwogli <mathiaskohler@web.de>
2024-07-05 10:38:38 +0200

    Edit disable login btn

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  listExcludedRoutes: Array<string> = ['/login'];
 
+  constructor(private router: Router) {}
+
+  shouldDisplayLoginButton(): boolean {
+    const currentRoute = this.router.routerState.snapshot.url;
+    const excludedRoutes = this.listExcludedRoutes;
+
+    return !excludedRoutes.includes(currentRoute);
+  }
 }
