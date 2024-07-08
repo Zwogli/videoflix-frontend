fc251028b0e30f17bf0f07505f4c864e9777f990
Zwogli <mathiaskohler@web.de>
2024-07-08 09:13:10 +0200

    Add error msg's

--- src/app/features/card-register/components/card-register/card-register.component.ts
+++ src/app/features/card-register/components/card-register/card-register.component.ts
@@ -3,8 +3,67 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-register',
   templateUrl: './card-register.component.html',
-  styleUrls: ['./card-register.component.scss']
+  styleUrls: ['./card-register.component.scss'],
 })
 export class CardRegisterComponent {
+  email: string = '';
+  username: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
+  emailError: boolean = false;
+  passwordError: boolean = false;
+  passwordComplexityError: boolean = false;
+  confirmPasswordError: boolean = false;
+
+  onSubmit(): void {
+    this.resetErrors();
+
+    if (!this.isValidEmail(this.email)) {
+      this.emailError = true;
+    }
+
+    if (!this.isValidPassword(this.password)) {
+      this.passwordError = true;
+    }
+
+    if (!this.isComplexPassword(this.password)) {
+      this.passwordComplexityError = true;
+    }
+
+    if (this.password !== this.confirmPassword) {
+      this.confirmPasswordError = true;
+    }
+
+    if (
+      !this.emailError &&
+      !this.passwordError &&
+      !this.passwordComplexityError &&
+      !this.confirmPasswordError
+    ) {
+      // Form is valid, proceed with the registration process
+      console.log('Form is valid, proceed with registration');
+    }
+  }
+
+  resetErrors(): void {
+    this.emailError = false;
+    this.passwordError = false;
+    this.passwordComplexityError = false;
+    this.confirmPasswordError = false;
+  }
+
+  isValidEmail(email: string): boolean {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+
+  isValidPassword(password: string): boolean {
+    return password.length >= 6;
+  }
+
+  isComplexPassword(password: string): boolean {
+    const complexityPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
+    return complexityPattern.test(password);
+  }
 }
