9aed95b224758352c239b564bd64aa59a2192c15
Zwogli <mathiaskohler@web.de>
2024-07-08 08:37:33 +0200

    Add etyle error

--- src/app/features/card-landing/components/card-landing/card-landing.component.ts
+++ src/app/features/card-landing/components/card-landing/card-landing.component.ts
@@ -9,6 +9,7 @@ import { DataSharingService } from 'src/app/shared/services/data-sharing/data-sh
 })
 export class CardLandingComponent {
   email: string = ''; // Variable for the e-mail address
+  emailError: boolean = false; // Variable to track email error state
 
   constructor(
     private router: Router,
@@ -16,7 +17,21 @@ export class CardLandingComponent {
   ) {}
 
   navigateToLogin(): void {
-    this.dataSharingService.setEmail(this.email); // Set the e-mail address in the service
-    this.router.navigate(['/login']); // Navigate to login-page
+    if (this.isValidEmail(this.email) || this.isEmptyEmail(this.email)) {
+      this.dataSharingService.setEmail(this.email); // Set the e-mail address in the service
+      this.router.navigate(['/login']); // Navigate to login-page
+    } else {
+      this.emailError = true;
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    // Simple regex for email validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+
+  isEmptyEmail(email: string) {
+    return email === '';
   }
 }
