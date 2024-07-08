4b7688eef17670dfa5d883936525cdca251a4fb9
Zwogli <mathiaskohler@web.de>
2024-07-08 09:47:44 +0200

    Add reset password

--- src/app/features/card-loginHelp/components/card-login-help/card-login-help.component.ts
+++ src/app/features/card-loginHelp/components/card-login-help/card-login-help.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-login-help',
   templateUrl: './card-login-help.component.html',
-  styleUrls: ['./card-login-help.component.scss']
+  styleUrls: ['./card-login-help.component.scss'],
 })
 export class CardLoginHelpComponent {
+  email: string = '';
 
+  emailError: boolean = false;
+
+  sendMail() {
+    if (this.isValidEmail(this.email)) {
+      console.log('Email confirmed');
+      // todo send mail
+    } else {
+      this.emailError = true;
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    // Simple regex for email validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
 }
