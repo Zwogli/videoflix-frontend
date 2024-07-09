db5f1ad80ad96082e1934371d78ba8c79bea7867
Zwogli <mathiaskohler@web.de>
2024-07-09 08:31:45 +0200

    Init form validation

--- src/app/features/card-login/components/card-login/card-login.component.ts
+++ src/app/features/card-login/components/card-login/card-login.component.ts
@@ -8,10 +8,39 @@ import { DataSharingService } from 'src/app/shared/services/data-sharing/data-sh
 })
 export class CardLoginComponent {
   email: string = '';
+  password: string = '';
+
+  emailError: boolean = false;
+  passwordError: boolean = false;
 
   constructor(private dataSharingService: DataSharingService) {}
 
   ngOnInit(): void {
     this.email = this.dataSharingService.getEmail(); // Get the email-address from service
+  }
+
+  login() {
+    this.resetErrors();
+    if (!this.isValidEmail(this.email)) {
+      this.emailError = true;
+    }
+
+    if (!this.isValidPassword(this.password)) {
+      this.passwordError = true;
+    }
+  }
+
+  resetErrors(): void {
+    this.emailError = false;
+    this.passwordError = false;
+  }
+
+  isValidEmail(email: string): boolean {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+
+  isValidPassword(password: string): boolean {
+    return password.length >= 6;
   }
 }
