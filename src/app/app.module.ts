1e9b2e83101215ea8d03a009906889dff33e28e1
Zwogli <mathiaskohler@web.de>
2024-07-17 08:24:57 +0200

    New component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
+// Imports:
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+// Material design
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+// Declarations:
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
@@ -20,7 +27,7 @@ import { CardRegisterComponent } from './features/card-register/components/card-
 import { RegisterPageComponent } from './features/register-page/components/register-page/register-page.component';
 import { LoginHelpPageComponent } from './features/loginHelp-page/components/login-help-page/login-help-page.component';
 import { CardLoginHelpComponent } from './features/card-loginHelp/components/card-login-help/card-login-help.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogComponent } from './shared/components/dialog/dialog/dialog.component';
 
 @NgModule({
   declarations: [
@@ -39,8 +46,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RegisterPageComponent,
     LoginHelpPageComponent,
     CardLoginHelpComponent,
+    DialogComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule, MatProgressSpinnerModule,],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatSnackBarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
