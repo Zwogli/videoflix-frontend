0b57c468bdb87b5dcc7739018df99c90d84c0319
Zwogli <mathiaskohler@web.de>
2024-07-17 08:53:59 +0200

    Edit verification dialog

--- src/app/features/login-page/components/login-page/login-page.component.ts
+++ src/app/features/login-page/components/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { BreakpointObserverService } from '../../../../core/services/breakpoint-observer/breakpoint-observer.service';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { NotificationService } from 'src/app/shared/services/notification/notification.service';
 
 @Component({
   selector: 'app-login-page',
@@ -11,9 +13,19 @@ export class LoginPageComponent {
   isXSmall$!: Observable<boolean>;
   isXLarge$!: Observable<boolean>;
 
-  constructor(private breakpointObserverService: BreakpointObserverService) {}
+  constructor(
+    private breakpointObserverService: BreakpointObserverService,
+    private route: ActivatedRoute,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const message = params['message'];
+      if (message) {
+        this.notificationService.showDialog(message);
+      }
+    });
     this.isXSmall$ = this.breakpointObserverService.isXSmall();
     this.isXLarge$ = this.breakpointObserverService.isXLarge();
   }
