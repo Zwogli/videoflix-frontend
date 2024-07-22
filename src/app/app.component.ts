8c0db9e6dbea05fc8c1c1db93d0a3d23ee22ec0b
Zwogli <mathiaskohler@web.de>
2024-07-22 09:57:13 +0200

    Fix url create csrftoken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,44 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { CsrfTokenService } from './shared/services/csrf-token/csrf-token.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'videoflix-frontend';
+  private baseUrl: string = environment.baseUrl;
 
-  constructor(private http: HttpClient, private csrfTokenService: CsrfTokenService) {}
+  constructor(
+    private http: HttpClient,
+    private csrfTokenService: CsrfTokenService
+  ) {}
 
   ngOnInit() {
     this.loadCsrfToken();
   }
 
   private loadCsrfToken() {
-    this.http.get('/api/get-csrf-token/', { responseType: 'text' }).subscribe(
-      (token: string) => {
-        this.csrfTokenService.setToken(token);
-      },
-      error => {
-        console.error('Error loading CSRF token:', error);
-      }
-    );
+    this.http
+      .get(`${this.baseUrl}/auth/get-csrf-token/`, { responseType: 'text' })
+      .subscribe({
+        next: (token) => {
+          this.csrfTokenService.setToken(token);
+        },
+        error: (error) => {
+          console.error('Error loading CSRF token:', error);
+        },
+      });
   }
+  // (token: string) => {
+  //   this.csrfTokenService.setToken(token);
+  // },
+  // (error) => {
+  //   console.error('Error loading CSRF token:', error);
+  // }
+  // );
+  // }
 }
