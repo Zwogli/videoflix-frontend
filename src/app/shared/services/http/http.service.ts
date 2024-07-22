9c92b6c19a865bf4f4ff545000f9cb4e241700c7
Zwogli <mathiaskohler@web.de>
2024-07-22 09:43:42 +0200

    Update headers csrftoken

--- src/app/shared/services/http/http.service.ts
+++ src/app/shared/services/http/http.service.ts
@@ -3,36 +3,54 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from 'src/app/models/user.model';
+import { CsrfTokenService } from '../csrf-token/csrf-token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  private baseUrl: string = environment.baseUrl;
 
-  private baseUrl:string = environment.baseUrl;
-
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private csrfTokenService: CsrfTokenService
+  ) {}
 
   private getHeaders(): HttpHeaders {
-    return new HttpHeaders({
+    let headers = new HttpHeaders({
       'Content-Type': 'application/json',
       // Add other headers if necessary
     });
+
+    const csrfToken = this.csrfTokenService.getToken();
+    if (csrfToken) {
+      headers = headers.set('X-CSRFToken', csrfToken);
+    }
+
+    return headers;
   }
 
   get<T>(endpoint: string): Observable<T> {
-    return this.http.get<T>(`${this.baseUrl}/${endpoint}`, { headers: this.getHeaders() });
+    return this.http.get<T>(`${this.baseUrl}/${endpoint}`, {
+      headers: this.getHeaders(),
+    });
   }
 
   post<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.post<T>(`${this.baseUrl}/${endpoint}`, body, { headers: this.getHeaders() });
+    return this.http.post<T>(`${this.baseUrl}/${endpoint}`, body, {
+      headers: this.getHeaders(),
+    });
   }
 
   put<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.put<T>(`${this.baseUrl}/${endpoint}`, body, { headers: this.getHeaders() });
+    return this.http.put<T>(`${this.baseUrl}/${endpoint}`, body, {
+      headers: this.getHeaders(),
+    });
   }
 
   delete<T>(endpoint: string): Observable<T> {
-    return this.http.delete<T>(`${this.baseUrl}/${endpoint}`, { headers: this.getHeaders() });
+    return this.http.delete<T>(`${this.baseUrl}/${endpoint}`, {
+      headers: this.getHeaders(),
+    });
   }
 }
