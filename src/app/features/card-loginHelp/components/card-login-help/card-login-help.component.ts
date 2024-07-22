386e9d5fe6907fda9c810516e87f8493ffe95713
Zwogli <mathiaskohler@web.de>
2024-07-22 09:29:23 +0200

    Edit post request reset password

--- src/app/features/card-loginHelp/components/card-login-help/card-login-help.component.ts
+++ src/app/features/card-loginHelp/components/card-login-help/card-login-help.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ValidationService } from 'src/app/shared/validation/validation.service';
+import { HttpService } from 'src/app/shared/services/http/http.service';
 
 @Component({
   selector: 'app-card-login-help',
@@ -11,13 +12,27 @@ export class CardLoginHelpComponent {
 
   emailError: boolean = false;
 
-  constructor(private validService: ValidationService) {}
+  constructor(
+    private validService: ValidationService,
+    private httpService: HttpService
+  ) {}
 
   sendMail() {
     if (this.validService.isValidEmail(this.email)) {
       this.emailError = false;
       console.log('Email confirmed');
       // todo send mail
+      this.httpService.post('auth/send-reset-email/', this.email).subscribe({
+        next: (response) => {
+          console.log('E-Mail zum Zurücksetzen des Passworts gesendet:', response);
+          // Hier kannst du eine Erfolgsmeldung anzeigen oder den Benutzer weiterleiten
+        },
+        error: (error) => {
+          console.error('Fehler beim Senden der E-Mail zum Zurücksetzen des Passworts:', error);
+          this.emailError = true;
+          // Hier kannst du eine Fehlermeldung anzeigen
+        }
+      });
     } else {
       this.emailError = true;
     }
