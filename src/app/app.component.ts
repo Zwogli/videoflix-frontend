5dcd67e87ff42abbcbe7c0401a1b5ec6ee27dc85
Zwogli <mathiaskohler@web.de>
2024-07-22 09:37:47 +0200

    Add create csrf-token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CsrfTokenService } from './shared/services/csrf-token/csrf-token.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'videoflix-frontend';
+
+  constructor(private http: HttpClient, private csrfTokenService: CsrfTokenService) {}
+
+  ngOnInit() {
+    this.loadCsrfToken();
+  }
+
+  private loadCsrfToken() {
+    this.http.get('/api/get-csrf-token/', { responseType: 'text' }).subscribe(
+      (token: string) => {
+        this.csrfTokenService.setToken(token);
+      },
+      error => {
+        console.error('Error loading CSRF token:', error);
+      }
+    );
+  }
 }
