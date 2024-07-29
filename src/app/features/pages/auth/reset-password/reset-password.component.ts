da73c74291bac00b72494f2cee5ff90e8c44151c
Zwogli <mathiaskohler@web.de>
2024-07-29 09:29:53 +0200

    Edit form passowrd user feedback

--- src/app/features/pages/auth/reset-password/reset-password.component.ts
+++ src/app/features/pages/auth/reset-password/reset-password.component.ts
@@ -10,21 +10,50 @@ import { HttpService } from 'src/app/shared/services/http/http.service';
 export class ResetPasswordComponent {
   password: string = '';
   confirmPassword: string = '';
+  validPassword: boolean = false;
+  validCofirmedPassword: boolean = false;
   passwordsMatch: boolean = false;
+  passwordErrorLength: boolean = false;
+  passwordErrorComplex: boolean = false;
 
   constructor(
     private validService: ValidationService,
     private httpService: HttpService
   ) {}
 
-  checkPasswords(): void {
-    this.passwordsMatch = this.password === this.confirmPassword;
+  confirmPasswordMatch() {
+    if (this.isPasswordMatch()) {
+      this.passwordsMatch = true;
+    } else {
+      this.passwordsMatch = false;
+    }
+  }
+
+  isPasswordMatch() {
+    return this.password === this.confirmPassword;
+  }
+
+  checkPassword() {
+    if (!this.validService.isValidPassword(this.password)) {
+      console.log('Passwortlänge: ', this.passwordErrorLength);
+
+      this.passwordErrorLength = true;
+    } else {
+      this.passwordErrorLength = false;
+    }
+    if (!this.validService.isComplexPassword(this.password)) {
+      this.passwordErrorComplex = true;
+    } else {
+      this.passwordErrorComplex = false;
+    }
+  }
+
+  isPasswordMinLength() {
+    return this.password.length && this.confirmPassword.length == 6;
   }
 
   submitChangePassword(): void {
-    if (this.passwordsMatch) {
-      // Hier können Sie Ihre Logik zum Ändern des Passworts hinzufügen
-      console.log('Passwörter stimmen überein. Passwort ändern.');
-    }
+    // Hier können Sie Ihre Logik zum Ändern des Passworts hinzufügen
+    console.log('Passwörter stimmen überein. Passwort ändern.');
   }
 }
