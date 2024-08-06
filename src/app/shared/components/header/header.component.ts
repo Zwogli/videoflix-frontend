5a889ac685e8bd3cf599bf43a15db64bcbe86f45
Zwogli <mathiaskohler@web.de>
2024-08-06 11:22:21 +0200

    Add transition

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +8,27 @@ import { Router, RouterStateSnapshot } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  listExcludedRoutes: Array<string> = ['/login'];
+  listExcludedRoutes: Array<string> = ['/login', '/home'];
+  dropdownOpen = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   isInvisibleLoginBtn(): boolean {
     const currentRoute = this.router.routerState.snapshot.url;
     const excludedRoutes = this.listExcludedRoutes;
 
     return !excludedRoutes.includes(currentRoute);
+  }
+
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  toggleDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  logout(): void {
+    this.authService.logout();
   }
 }
