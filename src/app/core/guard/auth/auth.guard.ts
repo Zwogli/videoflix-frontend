423a9d8701bed9d8d3931c535f01633273bb307b
Zwogli <mathiaskohler@web.de>
2024-08-06 10:36:39 +0200

    Edit authguard

--- src/app/core/guard/auth/auth.guard.ts
+++ src/app/core/guard/auth/auth.guard.ts
@@ -1,7 +1,21 @@
-import { CanActivateFn } from '@angular/router';
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const token = sessionStorage.getItem('authToken');
+
+    if (token) {
+      return true;
+    }
+
+    // Wenn nicht authentifiziert, zur Login-Seite umleiten
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
--- src/app/shared/services/interceptors/token.interceptor.ts
+++ src/app/shared/services/interceptors/token.interceptor.ts
@@ -1,21 +1,40 @@
-
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = sessionStorage.getItem('authToken');
+  constructor(private router: Router) {}
 
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = sessionStorage.getItem('authToken');
     if (token) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Token ${token}`,  // Authorization Header mit Token
+          Authorization: `Token ${token}`, // Authorization Header mit Token
         },
       });
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Leitet zur Startseite (oder Login-Seite) um, wenn nicht authentifiziert
+          this.router.navigate(['']);
+        }
+        return throwError(() => new Error(error.message));
+      })
+    );
   }
 }
