53f7a16b8e717342be07ae29046593739bc245c6
Zwogli <mathiaskohler@web.de>
2024-08-06 09:40:00 +0200

    Refactor interface

---
+++ src/app/shared/video/video.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Video } from '../../models/video.models';  // Importiere das Video-Interface
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VideoService {
+  private baseUrl: string = `${environment.baseUrl}/api/`;
+
+  constructor(private http: HttpClient) {}
+
+  getGlobalVideos(): Observable<Video[]> {
+    return this.http.get<Video[]>(`${this.baseUrl}global-videos/`);
+  }
+
+  getLocalVideos(): Observable<Video[]> {
+    return this.http.get<Video[]>(`${this.baseUrl}local-videos/`);
+  }
+}
