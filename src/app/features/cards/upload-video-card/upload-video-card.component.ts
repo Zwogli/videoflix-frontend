c4e98e4620fa7fa93c839a164da59164b1bfdcb8
Zwogli <mathiaskohler@web.de>
2024-08-12 11:32:15 +0200

    Edit style upload card

--- src/app/features/cards/upload-video-card/upload-video-card.component.ts
+++ src/app/features/cards/upload-video-card/upload-video-card.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-upload-video-card',
   templateUrl: './upload-video-card.component.html',
-  styleUrls: ['./upload-video-card.component.scss']
+  styleUrls: ['./upload-video-card.component.scss'],
 })
 export class UploadVideoCardComponent {
+  title: string = '';
+  description: string = '';
 
+  titleError: boolean = false;
+  descriptionError: boolean = false;
+  fileSelectorError: boolean = false;
+
+  loading: boolean = false;
+
+  onSubmit() {}
 }
