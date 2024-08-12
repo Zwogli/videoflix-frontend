aeb0c7ec0ffedaf89c81edf26c231c6748b81c11
Zwogli <mathiaskohler@web.de>
2024-08-12 11:45:13 +0200

    Edit disable btn style

--- src/app/features/cards/upload-video-card/upload-video-card.component.ts
+++ src/app/features/cards/upload-video-card/upload-video-card.component.ts
@@ -9,11 +9,43 @@ export class UploadVideoCardComponent {
   title: string = '';
   description: string = '';
 
+  fileSelector: File | null = null;
+
   titleError: boolean = false;
   descriptionError: boolean = false;
   fileSelectorError: boolean = false;
 
   loading: boolean = false;
 
-  onSubmit() {}
+  onSubmit() {
+    if (!this.isFormValid) {
+      // Fehler behandeln
+      this.titleError = this.title.trim() === '';
+      this.descriptionError = this.description.trim() === '';
+      this.fileSelectorError = this.fileSelector === null;
+      return;
+    }
+
+    this.loading = true;
+
+    // Implementiere hier die Logik zum Hochladen des Videos
+  }
+
+  get isFormValid(): boolean {
+    return (
+      this.title.trim() !== '' &&
+      this.description.trim() !== '' &&
+      this.fileSelector !== null
+    );
+  }
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.fileSelector = file;
+      this.fileSelectorError = false;
+    } else {
+      this.fileSelectorError = true;
+    }
+  }
 }
