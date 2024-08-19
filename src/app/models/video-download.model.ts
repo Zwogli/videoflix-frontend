9df40d343283ec18818428e587e0178046ca175a
Zwogli <mathiaskohler@web.de>
2024-08-19 08:27:20 +0200

    Edit videoDownload model

--- src/app/models/video-download.model.ts
+++ src/app/models/video-download.model.ts
@@ -1,7 +1,8 @@
 export interface VideoDownload {
-    id: number;
-    title: string;
-    description: string;
-    thumbnail: string;
-    file: string;
-  }
+  id: number;
+  title: string;
+  description: string;
+  thumbnail: string;
+  file: string;
+  isLocal: boolean;
+}
