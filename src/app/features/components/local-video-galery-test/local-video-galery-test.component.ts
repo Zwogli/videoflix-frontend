cf0d3ae3c6e1199773dc11e26d5458466d1b5993
Zwogli <mathiaskohler@web.de>
2024-11-19 15:09:56 +0100

    Create polling test

--- src/app/features/components/local-video-galery-test/local-video-galery-test.component.ts
+++ src/app/features/components/local-video-galery-test/local-video-galery-test.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { VideoDownload } from '../../../models/video-download.model';
-import { LocalThumbnailService } from 'src/app/core/services/local-thumbnail/local-thumbnail.service';
 import { ActivatedRoute } from '@angular/router'; // Catch video_id
-import { interval, switchMap, tap } from 'rxjs';
+import { interval, switchMap, takeWhile } from 'rxjs';
+import { PollingService } from '../../../core/services/polling/polling.service';
 
 @Component({
   selector: 'app-local-video-galery-test',
@@ -10,21 +10,43 @@ import { interval, switchMap, tap } from 'rxjs';
   styleUrls: ['./local-video-galery-test.component.scss']
 })
 export class LocalVideoGaleryTestComponent {
-
   @Input() localVideos: VideoDownload[] = [];
   @Output() play = new EventEmitter<VideoDownload>();
+
   defaultThumbnail: string =
     'https://videoflix-server.mathias-kohler.de/static/images/coming-soon.jpg?v=1';
-    // * Testpolling
-  videoId?: number;
-  thumbnailCreated: boolean = false;
+
 
   constructor(
     private route: ActivatedRoute,
+    private pollingService: PollingService
   ) {}
  
 
+  ngOnInit(): void {
+    this.startPollingForThumbnails();
+  }
+
+
   playVideo(video: VideoDownload): void {
     this.play.emit(video);
+  }
+
+
+  startPollingForThumbnails(): void {
+    this.localVideos.forEach((video) => {
+      if (!video.thumbnail) {
+        interval(5000) // Alle 5 Sekunden
+          .pipe(
+            switchMap(() => this.pollingService.checkThumbnailStatus(video.id)),
+            takeWhile((response) => !response.thumbnail_created, true) // Beende Polling, wenn Thumbnail erstellt ist
+          )
+          .subscribe((response) => {
+            if (response.thumbnail_created) {
+              video.thumbnail = response.thumbnail; // Aktualisiere das Thumbnail
+            }
+          });
+      }
+    });
   }
 }
